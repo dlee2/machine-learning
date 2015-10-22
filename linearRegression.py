bb3f0dee52d3ffbaea501b3a18f488cbeb8fd96d
Daniel Lee <Dan@Daniels-MacBook-Air-3.local>
2015-10-22 17:36:06 -0400

    added packages

---
+++ linearRegression.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 17:31:48 2015
+website:
+http://connor-johnson.com/2014/02/18/linear-regression-with-python/
+@author: Dan
+"""
+
+import numpy as np
+import pandas as pd
+import statsmodels
+from sklearn.linear_model import LinearRegression
+import scipy,scipy.stats
