9ec9af0d88f17ed7e54b4c670c14324b17074041
Daniel Lee <Dan@Daniels-MacBook-Air-3.local>
2015-10-22 17:19:58 -0400

    full tutorial

--- randomForest.py
+++ randomForest.py
@@ -14,4 +14,17 @@ import numpy as np
 iris = load_iris()
 df = pd.DataFrame(iris.data, columns = iris.feature_names)
 df['train'] = np.random.uniform(0,1,len(df)) <= .75
-df['species'] = pd.Factor(iris.target, iris.target_names)
+df['species'] = pd.Categorical.from_codes(iris.target, iris.target_names)
+
+df.head()
+
+train, test = df[df['train']== True], df[df['train']==False]
+
+features = df.columns[:4]
+clf = RandomForestClassifier(n_jobs = 2)
+
+y,_ = pd.factorize(train['species'])
+clf.fit(train[features], y)
+
+preds = iris.target_names[clf.predict(test[features])]
+pd.crosstab(test['species'], preds, rownames=['actual'], colnames=['preds'])
