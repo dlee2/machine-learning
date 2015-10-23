f5d2bb1cdad1d63e126f327046c7579aaf1ff335
Daniel Lee <Dan@Daniels-MacBook-Air-3.local>
2015-10-23 09:36:05 -0400

    added more

--- linearRegression.py
+++ linearRegression.py
@@ -11,3 +11,22 @@ import pandas as pd
 import statsmodels
 from sklearn.linear_model import LinearRegression
 import scipy,scipy.stats
+
+data_str = '''Region Alcohol Tobacco
+North 6.47 4.03
+Yorkshire 6.13 3.76
+Northeast 6.19 3.77
+East Midlands 4.89 3.34
+West Midlands 5.63 3.47
+East Anglia 4.52 2.92
+Southeast 5.89 3.20
+Southwest 4.79 2.71
+Wales 5.27 3.53
+Scotland 6.08 4.51
+Northern Ireland 4.02 4.56'''
+
+d = data_str.split('\n')
+d = [i.split(' ') for i in d]
+
+
+df = pd.DataFrame(d[1:], columns = d[0])
